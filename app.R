f4ba8601f86a56a838dae36cffaed4310096beeb
Zhang Hong <89785404+Crispy-tuna@users.noreply.github.com>
2021-09-28 23:14:03 +0200

    Add files via upload

---
+++ app.R
@@ -0,0 +1,41 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(devtools)
+devtools::install_github("Crispy-tuna/lab5group5")
+library(lab5group5)
+
+ui <- fluidPage(
+    # Tittle and widgets
+    pageWithSidebar(
+        headerPanel("Input data"),
+        sidebarPanel(
+          textInput("name",label="Name"),
+          textInput("zoom_level", label="Zoom level"),
+          selectInput("select", label="Choose a type of map", 
+                                  choices =c("toner" , "terrain",
+                                                 "watercolor"),
+                                  selected = "toner")
+        ),
+
+        mainPanel(
+          imageOutput("map")
+        )
+    )
+)
+
+server <- function(input, output) {
+
+    output$map <- renderPlot({
+      lab5group5::openmap_by_name(input$name,input$zoom_level,input$select)})
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
